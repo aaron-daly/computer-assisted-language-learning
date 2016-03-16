64460dc51c561da48dd5b4aa0285d5b530c4198b
raphaelle smyth <raphaelle smyth>
2016-03-16 16:24:00 +0000

    change

--- public/js/controllers/ScenarioGameCtrl.js
+++ public/js/controllers/ScenarioGameCtrl.js
@@ -11,14 +11,17 @@ angular.module('ScenarioGameCtrl', []).controller('ScenarioGameController', ['$s
         $('#replay-btn').hide();
         $('#return-btn').hide();
 
-        $scope.scenario = scenarioGame.scenario; //type in here
+        $scope.scenario = scenarioGame.scenario;
 
+        //draws the answer in the answer containner.
         $scope.drawAnswer = function(answer) {
-            var html = '<button class="btn btn-info answer-btn" value="' + answer.branch + '">' + answer.answer + '</button>';
+            var html = '<button class="btn btn-info answer-btn" /*!--value="' + answer.branch + '"*/>' + answer.answer + '</button>';
             $('#answer-container').append(html);
         };
 
         var count=0;
+        //draws the question in the current question div.
+        //condition set for if word type game is chosen.
         $scope.drawQuestion = function(question) {
             if ($scope.scenario.scenarioType.name == "Word") {
                 var questionHtml = '<h2>' + question.question + '<img src="../images/wordSample/Image' + count + '.jpg" style="width:50px;height:50px;display:inline;"/>' + '</h2>';
@@ -34,21 +37,20 @@ angular.module('ScenarioGameCtrl', []).controller('ScenarioGameController', ['$s
             });
         };
 
-        // TODO SHOW TRANSLATIONS ON RESULTS PAGE...
+        //draws translation if translations are enabled by the teacher & if there are any
         $scope.drawTranslations = function(translation) {
-            console.log(scenarioGame.scenario.conversation[scenarioGame.position -1].translation);//!!!!!!!!!!!!!!!!!!!
             if (scenarioGame.scenario.conversation[scenarioGame.position -1].translation){
                  var translationHTML = '<h2>' + translation.translation + '</h2>';
                  $('#current-question').append(translationHTML);
              }
         };
+
         var questionCount=1;
         var levelCount=0;
+        //shows the audio for the question
         $scope.showAudio = function() {
             $('#scenarioGame-audio').empty();
             if ($scope.scenario.scenarioType.name == "Conversation"){
-                //$('#scenarioGame-audio').show();
-                console.log($scope.scenario.level);
                 levelCount = $scope.scenario.level;
                 var audioHTML = '<h2>' + '<audio controls>'+ '<source src="../audio/sweetshop/level'+levelCount+'/q'+questionCount+'.wav" type="audio/wav">'
                     + '</audio>' + '</h2>';
@@ -58,42 +60,39 @@ angular.module('ScenarioGameCtrl', []).controller('ScenarioGameController', ['$s
             }
         };
 
+        //clears the screen
         $scope.clearScreen = function() {
             $('#answer-container').empty();
             $('#current-question').empty();
         };
 
+        //takes next question and calls function to draw it
+        //send answer clicked to service to make an array of students answers
         $scope.tick = function(answer){
             scenarioGame.tick(answer, function(data) {
                 $scope.clearScreen();
-
-
                 if(data.question) {
                     $('#scenarioGame-audio').hide();
                     $scope.drawQuestion(data);
                     questionCount++;
-
-                    $scope.drawTranslations(data);                  //!!!!!!!!!!!!!!!!!
-
+                    $scope.drawTranslations(data);
                     $('.answer-btn').click(function () {
-
                         var answer = $(this).text();
-
                         $scope.tick({
                             answer: answer,
                         });
                     });
-                } else {
+                }
+                else {
                     $scope.finish(data);
                 }
-
             });
         };
-
-
+        //retrieves arrays from service of all questions and answers
+        //retrieves array of correct answers and array of translations
         $scope.finish = function(results) {
             questionCount=0;
-
+            //If game is completed..
             var isCompleted = true;
             if(results.correct) {
                 for (var i = 0; i < results.correct.length; i++) {
@@ -102,16 +101,14 @@ angular.module('ScenarioGameCtrl', []).controller('ScenarioGameController', ['$s
                     }
                 }
             }
-
             var isCompleted = true;
             if(results.translations) {
                 for (var i = 0; i < results.translations.length; i++) {
-                    if (!results.translations[i]) {                                                         //!!!!!!!!!
+                    if (!results.translations[i]) {
                         isCompleted = false;
                     }
                 }
             }
-
             var isCompleted = true;
             if(results.correctAnswers) {
                 for (var i = 0; i < results.correctAnswers.length; i++) {
@@ -120,8 +117,6 @@ angular.module('ScenarioGameCtrl', []).controller('ScenarioGameController', ['$s
                     }
                 }
             }
-
-            // if game completed...
             if(isCompleted) {
                 // authorize permission level 2 (pupil/student)
                 auth.authorizePermission(2, function(authorized) {
@@ -130,21 +125,16 @@ angular.module('ScenarioGameCtrl', []).controller('ScenarioGameController', ['$s
                     }
                 });
             }
-
             var questionHTML = 0;
             var answerHTML = 0;
-
             $.each(results.questions, function(key, val) {
                 questionHTML = '<h2>' + val + '</h2>';
-
                 //if the correct value in corresponding position of array is true then ....
                 if (results.correct.length != 0) {
-                    //if results.correct is true print question and right answer.
                     if (results.correct[key]) {
                         answerHTML = '<h4 class="text-success" style= "color:green" >'
                             + results.answers[key] + '<span class="glyphicon glyphicon-ok"></span>' + '</h4>';
                     }
-                    //if false print question answer and correct answer.
                     else {
                         answerHTML = '<h4 class="text-success" style= "color:red" >' + results.answers[key] +
                             '<span class="glyphicon glyphicon-remove"></span>' +
@@ -163,43 +153,30 @@ angular.module('ScenarioGameCtrl', []).controller('ScenarioGameController', ['$s
             $('#replay-btn').show();
             $('#return-btn').show();
         };
-
-
-
-
-
         $scope.gameReplay = function() {
             $route.reload();
         };
-
         $scope.profileReturn = function() {
             $location.path('/profile');
         };
-
-
+        //When the game begins
         scenarioGame.initGame(function(data) {
             $('#scenarioGame-audio').hide();
-        //condition for picture scenario
-          if ($scope.scenario.scenarioType.name != "Picture"){
+            //condition for picture scenario
+            if ($scope.scenario.scenarioType.name != "Picture"){
                 $('#scenarioGame-picture').hide();
             }
             else {
-             $('#scenarioGame-picture').show();
+                $('#scenarioGame-picture').show();
             }
-
             $scope.drawQuestion(data);
-
             $('.answer-btn').click(function(){
-
                 var answer = $(this).text();
-
                 $scope.tick({
                     answer: answer
                 });
             });
-
         });
-
     }
 ]);
 
--- public/js/services/scenarioGame.js
+++ public/js/services/scenarioGame.js
@@ -6,10 +6,9 @@ angular.module('calliApp')
     .factory('scenarioGame', ['$http', 'scenario',
         function($http, scenario){
 
+            //How scenarios are seen throughout the program
             var scenarioGame = {
-
                 scenario: {},
-
                 scenarioList: [],
                 position: 1,
                 recordedQuestions: [],
@@ -19,54 +18,37 @@ angular.module('calliApp')
                 recordedTranslations:[],
                 isPlaying: false
             };
-
-
             //initiate game, callback first question
             scenarioGame.initGame = function(callback) {
-
-
                 this.isPlaying = true;
                 var firstQuestion = this.nextQuestion();
                 callback(firstQuestion);
 
             };
-
             //receives answer, calls back next question
             scenarioGame.tick = function(answer, callback) {
                 $('#scenarioGame-audio').hide();
                 this.recordedAnswers.push(answer.answer);
-
                 var currentAnswers = scenarioGame.scenario.conversation[scenarioGame.position - 1].answers;
                 if(scenarioGame.scenario.conversation[scenarioGame.position -1].translation) {
                     var translationAnswers = scenarioGame.scenario.conversation[scenarioGame.position -1].translation;
-                    scenarioGame.recordedTranslations.push(translationAnswers); //filing array with translations
+                    //filing array with translations
+                    scenarioGame.recordedTranslations.push(translationAnswers);
                 }
-                    // console.log(val.answer);
-
-
-
-                //answer,branch,correct of current answer.
-                //if the scenario type is conversation dont push correct answers
+                //if the scenario type is not conversation fill array with correct boolean values
                 if(scenarioGame.scenario.scenarioType.name != "Conversation"){
-
                     //if correct=true fill correct answers array.
                     $.each(currentAnswers, function (key, val) {
-                            console.log(val.answer);
                         if(val.correct){
                             scenarioGame.recordedCorrect.push(val.answer);
-                            // console.log(val.answer);
                         }
                         if (val.answer == answer.answer) {
-
                             scenarioGame.recordedCorrectAnswers.push(val.correct);
-                            //console.log(val.correct);
                         }
                     });
                 }
-
                 //if we reach delimiter position 0, callback results and release game
                 this.position++;
-
                 if(this.position > this.scenario.conversation.length) {
                     callback(this.getResults());
                     this.release();
@@ -74,9 +56,7 @@ angular.module('calliApp')
                     //update position and callback next question
                     callback(this.nextQuestion());
                 }
-
             };
-
             //results of questions and answers
             scenarioGame.getResults = function() {
                 return {
@@ -87,7 +67,6 @@ angular.module('calliApp')
                     translations: this.recordedTranslations
                 }
             };
-
             //release game data
             scenarioGame.release = function() {
                 this.scenario={};
@@ -98,21 +77,17 @@ angular.module('calliApp')
                 this.recordedCorrectAnswers = [];
                 this.recordedTranslations =[];
             };
-
             //load question of game's current position
             scenarioGame.nextQuestion = function() {
                 var question = this.scenario.conversation[scenarioGame.position-1];
                 this.recordedQuestions.push(question.question);
                 return question;
             };
-
             //loaf scenario for the game
             scenarioGame.loadScenario = function(scenarioName, callback) {
-
                 var i = 0;
                 var len = scenarioGame.scenarioList.length;
                 var data = {};
-
                 for(i; i < len; i++) {
                     if(scenarioGame.scenarioList[i].name === scenarioName) {
                         data = scenarioGame.scenarioList[i];
@@ -120,12 +95,9 @@ angular.module('calliApp')
                         callback(data);
                     }
                 }
-
             };
-
             //preloads games into scenarioList
-            scenarioGame.preload = function() {   //preload?
-
+            scenarioGame.preload = function() {
                 scenario.getScenarios(function(data) {
                     if(scenarioGame.scenarioList.length == 0){
                         $.each(data, function(key, scenario) {
@@ -134,22 +106,18 @@ angular.module('calliApp')
                     }
                 });
             };
-
             //return array of names of scenarios in scenarioList
             scenarioGame.scenarioListNames = function() {
                 var arr = [];
                 var i = 0;
                 var len = scenarioGame.scenarioList.length;
-
                 for(i; i < len; i++) {
                     arr.push(scenarioGame.scenarioList[i].name);
                 }
-
                 return arr;
             };
-
             // return true if game scenarioList contains a scenario with the name given, else return false
-           scenarioGame.containsScenario = function(scenarioName) {
+            scenarioGame.containsScenario = function(scenarioName) {
                 if(scenarioGame.scenarioList) {
                     var i = 0;
                     var len = scenarioGame.scenarioList.length;
@@ -161,17 +129,14 @@ angular.module('calliApp')
                 }
                 return false;
             };
-
             // get info of a scenario by scenarioId
             scenarioGame.getScenarioInfo = function(scenarioId) {
-
                 $.each(scenarioGame.scenarioList, function(key, val) {
                     if(val._id === scenarioId) {
                         return val;
                     }
                 });
             };
-
             return scenarioGame;
         }
     ]);
