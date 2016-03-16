34c4103169fed3b65ba211f6763dfdeec65d82f5
dalya42 <aaron.daly42@mail.dcu.ie>
2016-03-16 16:03:33 +0000

    code commented, about page layout

--- public/js/controllers/AboutCtrl.js
+++ public/js/controllers/AboutCtrl.js
@@ -2,6 +2,7 @@
 angular.module('AboutCtrl', []).controller('AboutController', ['$scope', '$location',
     function($scope, $location) {
 
+        // direct to register page
         $scope.directToRegister = function() {
             $location.path('/register');
         }
